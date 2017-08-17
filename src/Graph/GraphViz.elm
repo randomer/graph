cd529ad1e72627256a5e15ec9210ba94f18d96e7
Jeff Schomay <jschomay@gmail.com>
2017-08-16 19:27:08 -0600

    Add support for graphviz attrs

--- src/Graph/GraphViz.elm
+++ src/Graph/GraphViz.elm
@@ -1,14 +1,24 @@
-module Graph.GraphViz exposing (output)
+module Graph.GraphViz
+    exposing
+        ( output
+        , outputWithStyles
+        , defaultStyles
+        , Styles
+        )
 
 {-| This module provides a means of converting the `Graph` data type into a valid [GraphViz](http://www.graphviz.org/) string for visualizing your graph structure.
 
- You can easily preview your graph by inserting the generated string into an online GraphViz tool like https://dreampuf.github.io/GraphvizOnline/.
+You can easily preview your graph by inserting the generated string into an online GraphViz tool like https://dreampuf.github.io/GraphvizOnline/.
 
- You can also dynamically draw your graph in your application by sending the string over a port to the javascript version of the GraphViz library, https://github.com/mdaines/viz.js/ (see the examples there fore more specifics on how to embed the generated visualization).
-
-Note that this currently only supports a subset of the full GraphViz language.
+You can also dynamically draw your graph in your application by sending the string over a port to the javascript version of the GraphViz library, https://github.com/mdaines/viz.js/ (see the examples there fore more specifics on how to embed the generated visualization).
 
 @docs output
+
+# Attrs
+
+GraphViz allows for customizing the graph's look via "Attrs."
+
+@docs Styles, defaultStyles, outputWithStyles
 -}
 
 import Graph exposing (Graph, Edge, Node, edges, nodes, get)
@@ -17,7 +27,37 @@ import Graph exposing (Graph, Edge, Node, edges, nodes, get)
 {-| Converts a `Graph` into a valid GraphViz string.  Note that the you must supply a `Graph String e` type, where the `String` is the label that should be printed in each node.
 -}
 output : Graph String e -> String
-output graph =
+output =
+    outputWithStyles defaultStyles
+
+
+{-| A type representing the attrs to apply at the graph, node, and edge entities (subgraphs and cluster subgraphs are not supported).
+
+Note that `Styles` is made up of strings, which loses type safety, but allows you to use any GraphViz attrs without having to model them out in entirety in this module.  It is up to you to make sure you provide valid attr strings.  See http://www.graphviz.org/content/attrs for available options.
+-}
+type alias Styles =
+    { graph : String
+    , node : String
+    , edge : String
+    }
+
+
+{-| A blank `Styles` record to build from to define your own styles.
+
+    myStyles =
+        { defaultStyles
+            | node = "shape=box, color=blue, style=\"rounded, filled\""
+        }
+-}
+defaultStyles : Styles
+defaultStyles =
+    Styles "" "" ""
+
+
+{-| Same as `output`, but allows you to add attrs to the graph.  These attrs will be applied to the entire graph.
+-}
+outputWithStyles : Styles -> Graph String e -> String
+outputWithStyles styles graph =
     let
         getText id =
             get id graph
@@ -45,7 +85,7 @@ output graph =
 
         edgesString =
             List.map edge edges
-                |> String.join ";\n"
+                |> String.join "\n"
 
         edge ({ from, to, label } as edge) =
             "  "
@@ -55,9 +95,28 @@ output graph =
 
         nodesString =
             List.map node nodes
-                |> String.join ";\n"
+                |> String.join "\n"
 
         node node =
             "  " ++ Basics.toString node.label
+
+        graphStyles =
+            "  graph [" ++ styles.graph ++ "]"
+
+        nodeStyles =
+            "  node [" ++ styles.node ++ "]"
+
+        edgeStyles =
+            "  edge [" ++ styles.edge ++ "]"
     in
-        "digraph G {\n" ++ edgesString ++ ";\n\n" ++ nodesString ++ ";\n}"
+        "digraph G {\n"
+            ++ graphStyles
+            ++ "\n"
+            ++ nodeStyles
+            ++ "\n"
+            ++ edgeStyles
+            ++ "\n\n"
+            ++ edgesString
+            ++ "\n\n"
+            ++ nodesString
+            ++ "\n}"
--- tests/Tests/Graph/GraphViz.elm
+++ tests/Tests/Graph/GraphViz.elm
@@ -1,6 +1,6 @@
 module Tests.Graph.GraphViz exposing (all)
 
-import Graph.GraphViz as Viz exposing (output)
+import Graph.GraphViz as Viz exposing (..)
 import Graph exposing (Graph, NodeId, Node, Edge, NodeContext)
 import Test exposing (..)
 import Expect
@@ -33,18 +33,69 @@ all =
 
                     expected =
                         """digraph G {
-  "Welcome" -> "To";
-  "To" -> "Web";
-  "To" -> "GraphViz!";
+  graph []
+  node []
+  edge []
 
-  "Welcome";
-  "To";
-  "Web";
-  "GraphViz!";
+  "Welcome" -> "To"
+  "To" -> "Web"
+  "To" -> "GraphViz!"
+
+  "Welcome"
+  "To"
+  "Web"
+  "GraphViz!"
 }"""
 
                     actual =
                         output g
+                in
+                    \() -> Expect.equal expected actual
+            , test "with styles" <|
+                let
+                    nodes =
+                        [ Node 0 "Welcome"
+                        , Node 1 "To"
+                        , Node 2 "Web"
+                        , Node 3 "GraphViz!"
+                        ]
+
+                    e from to =
+                        Edge from to ()
+
+                    edges =
+                        [ e 0 1
+                        , e 1 2
+                        , e 1 3
+                        ]
+
+                    g =
+                        Graph.fromNodesAndEdges nodes edges
+
+                    expected =
+                        """digraph G {
+  graph [bgcolor=red]
+  node [shape=box, color=blue, style="rounded, filled"]
+  edge []
+
+  "Welcome" -> "To"
+  "To" -> "Web"
+  "To" -> "GraphViz!"
+
+  "Welcome"
+  "To"
+  "Web"
+  "GraphViz!"
+}"""
+
+                    myStyles =
+                        { defaultStyles
+                            | graph = "bgcolor=red"
+                            , node = "shape=box, color=blue, style=\"rounded, filled\""
+                        }
+
+                    actual =
+                        outputWithStyles myStyles g
                 in
                     \() -> Expect.equal expected actual
             ]
